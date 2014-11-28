5adf8fd0c01b2fe0f5743c15db81c541352c78fe
Sandro Rodrigues <sfr@eurotux.com>
2014-11-28 15:14:52 +0000

    Added exmple and settings to plugin

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ laziness
 ========
 
 jQuery plugin to images lazy loading.
+
+Example:
+--------
+
+::
+    $(document).ready(function() {
+
+        $(document).laziness({
+            selector: 'img[data-src]', // image selector on selected container
+            attribute: 'data-src',     // imagesrc html attribute
+            onload: null,              // callback called on each image load
+            threshold: 200,            // load images earlier, ex: 200px earlier
+        });        
+
+    });
--- jquery.laziness.js
+++ jquery.laziness.js
@@ -1,30 +1,36 @@
 ;(function($) {
-    $.fn.laziness = function(imgs, callback) {
+    $.fn.laziness = function(options) {
+
+        var settings = $.extend({
+            // These are the defaults.
+            selector: 'img[data-src]',
+            attribute: 'data-src',
+            onload: null,
+            threshold: 200,
+            
+        }, options);
 
         return this.each(function(index, elem) {
 
             var $this = $(elem),
-                images = imgs? imgs: $this.find('img'),
+                images = $this.find(settings.selector),
                 loaded;
 
             images.one("laziness", function() {
 
-                var source = this.getAttribute("data-src");
+                var source = this.getAttribute(settings.attribute);
                 var img = this;
 
                 if (source) {
                     img.onload = function () {
-                        if (typeof callback === "function") {
-                            callback.call(img);
-                        }
+                        if (typeof settings.onload === "function")
+                            settings.onload.call(img);
                     };
-
                     img.src = source;
                 }
             });
 
-            function laziness()
-            {
+            var laziness = function () {
                 var inview = images.filter(function() {
                     var rect = this.getBoundingClientRect();
                     var $e = $(this);
@@ -34,7 +40,7 @@
                     return (
                         rect.top >= 0
                         && rect.left >= 0
-                        && (rect.top - 200) <= (window.innerHeight || document.documentElement.clientHeight)
+                        && (rect.top - settings.threshold) <= (window.innerHeight || document.documentElement.clientHeight)
                         && (rect.left-window.innerWidth) <= (window.innerWidth || document.documentElement.clientWidth)
                     )
                 });
